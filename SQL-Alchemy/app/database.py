9ba9d09bfee21a710c141276d6a29e038dba74b4
Mohith <117433897+MohithChilakala@users.noreply.github.com>
2024-02-18 21:13:03 +0530

    Update database.py

--- SQL-Alchemy/app/database.py
+++ SQL-Alchemy/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from urllib.parse import quote_plus
 
-SQLALCHEMY_DATABASE_URL = 'mysql://root:%s@localhost/fast_api' % quote_plus("Mohith@4124")
+SQLALCHEMY_DATABASE_URL = 'mysql://username:%s@localhost/fast_api' % quote_plus('password')
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
